56a8031aebec51c18fc95b263fee4d79f682a628
Guillermo Villacura <g.villacurat@gmail.com>
2023-04-09 19:08:00 -0400

    Clase 05-04-2023

---
+++ pagina web JS/js/funciones.js
@@ -0,0 +1,59 @@
+console.log("Hola mundo");
+
+//asdasdasd
+
+let variableUno = 10;
+let variableDos = "10";
+
+console.log("Valor de la variable: ",variableUno, "dos ", variableDos);
+
+const constante = 10;
+    //constante = 20;
+console.log(constante);
+
+
+//alert("Bienvenidos");
+
+
+if (variableUno == variableDos) {
+    console.log("Son iguales");
+}else{
+    console.log("No son iguales");
+}
+
+if (variableUno !== variableDos) {
+    console.log("Son distintos");
+}else{
+    console.log("No son distintos");
+}
+
+
+if (variableUno > 5 && variableDos == 7) {
+    
+}
+
+if (variableUno < 5 || variableDos == 10) {
+    
+}
+
+
+
+if ("50" > 40) {
+    console.log("es mayor");
+}
+
+function miFuncion() {
+    console.log("Mi primera funcion.");
+}
+
+miFuncion();
+
+
+function sumar(n1,n2){
+    let resultado = n1 + parseInt(n2);
+    console.log("El resultado de la suyma es: ", resultado);
+}
+
+
+sumar(50,50);
+
