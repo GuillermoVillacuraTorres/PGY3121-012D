f0db211c15d46ace6efe66679b9d27ef0adf6f1b
Guillermo Villacura <g.villacurat@gmail.com>
2023-05-03 20:51:08 -0400

    Clase 03-05-2023

--- pagina web JS/js/form.js
+++ pagina web JS/js/form.js
@@ -54,3 +54,54 @@ function validarFormulario(){
 
 
 }
+
+
+const ocultar = document.getElementById("ocultar");
+ocultar.style.display = "none";
+
+function password(){
+    const mostrar = document.getElementById("mostrar");
+    const input = document.getElementById("txtPass");
+
+    if (input.type == "password") {
+        input.type = "text";
+        ocultar.style.display = "inline";
+        mostrar.style.display = "none";
+    }else{
+        input.type = "password";
+        ocultar.style.display = "none";
+        mostrar.style.display = "inline";
+    }
+}
+
+
+
+function cargarFormulario(){
+    const contenedor = document.getElementById("contenedor");
+    const row = document.createElement("div");
+    const col = document.createElement("div");
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+
+
+    row.classList.add("row");
+    col.classList.add("col");
+
+    label.textContent = "Nombre:";
+    input.classList.add("form-control");
+    input.type = "text";
+    input.placeholder = "Ingrese su nombre";
+    input.id = "txtNombreJs";
+    input.value = "1223";
+
+    contenedor.appendChild(row);
+    row.appendChild(col);
+    col.appendChild(label);
+    col.appendChild(input);
+
+    console.log("Valor input:",input.value);
+}
+
+function load(){
+    cargarFormulario();
+}
