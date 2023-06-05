70961cd8258c2ea12a96a03795ce866f3e2660bd
Guillermo Villacura <g.villacurat@gmail.com>
2023-06-04 22:16:45 -0400

    Clase 02-06-2023

--- django/apps/Tienda/views.py
+++ django/apps/Tienda/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 
 def cargarInicio(request):
-    return render(request,"inicio.html")
+    productos = Producto.objects.all()
+    return render(request,"inicio.html",{"prod" : productos})
